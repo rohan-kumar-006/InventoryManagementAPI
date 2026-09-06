12740755f7f556ecb0222dff69a3c864157b14f0
[Rohan Kumar] <rohanchauhan3845@gmail.com>
2026-09-07 00:48:03 +0530

    Fix login issue

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Login bug fixed
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
