bdf2070e8e47f0f188c54a3e9a4a8869e5539845
[Rohan Kumar] <rohanchauhan3845@gmail.com>
2026-09-07 11:52:26 +0530

    Add stock alert API endpoint

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //stock alert validation added 
 
+// Stock alert API endpoint added
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
